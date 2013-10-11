3c9d6e8b14f09854242a941458be48ea4658bbbd
jnquick <jamesquick@dyn-160-39-229-55.dyn.columbia.edu>
2013-10-10 23:17:13 -0400

    setup one mic, tried 4 mics
    I setup one mic based on the tutorial, then attempted to add 3 other
    mics, but I had other syntax errors

---
+++ app.js
@@ -0,0 +1,30 @@
+var five = require("johnny-five"),
+        board, microphone1;
+
+    board = new five.Board();
+
+    board.on("ready", function() {
+
+      // Create a new `microphone` hardware instance.
+      microphone1 = new five.Sensor({
+        pin: "A1",
+        freq: 250
+      });
+
+      // Inject the `sensor` hardware into
+      // the Repl instance's context;
+      // allows direct command line access
+      board.repl.inject({
+        pot: microphone1
+      });
+
+      // "data" get the current reading from the photoresistor
+      microphone1.on("data", function() {
+        console.log( this.value );
+      });
+    });
+
+
+    // References
+    //
+    // http://nakkaya.com/2009/10/29/connecting-a-photoresistor-to-an-arduino/
